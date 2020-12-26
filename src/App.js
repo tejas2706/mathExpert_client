4ff65d4cabda16757abc8c2ad7f6239921653e31
Murtuza B <murtuzabagasrawala@gmail.com>
2020-12-26 15:58:28 +0530

    footer added and removed overall scrollbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import Home from './Pages/Home';
+import Footer from './Components/Footer'
 
 function App() {
   return (
     <div className="app__root">
       <Navbar />
       <Home />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PageBreak from "../PageBreak";
+import "./styles.css"
+
+function Footer() {
+    return (
+        <div className="footer__container">
+            <div className="footer__links">
+                <a className="footer_link" href="#" >Contact Us </a>
+                <a className="footer_link" href="#" >About Us </a>
+                <a className="footer_link" href="#" >FAQ</a>
+                <a className="footer_link" href="#" >Privacy Policy</a>
+            </div>
+            <div className="footer__copyrights">
+                <p cl>Copyright © 2020-2040 OlympExpert, All rights reserved.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
