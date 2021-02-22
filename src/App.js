d1e653b840d8356596ae40a9480e3680966842d5
tejaslad <tejaslad2706@gmail.com>
2021-02-23 01:13:16 +0530

    Added quizResult Page

--- src/App.js
+++ src/App.js
@@ -9,18 +9,20 @@ import Content from './Pages/Content';
 import QuestionsCard from './Components/QuestionsCard';
 import QuizQuestion from './Components/QuizQuestion';
 import Quiz from './Pages/Quiz';
+import QuizResult from './Pages/QuizResult';
 
 function App() {
   return (
     <div className="app__root">
-      <Navbar />
+      {/* <Navbar /> */}
       {/* <TopicDetails /> */}
       {/* <Home/> */}
       {/* <Questions /> */}
       {/* <Quiz /> */}
       {/* <QuizQuestion /> */}
-      <Quiz />
-      <Footer />
+      {/* <Quiz /> */}
+      <QuizResult />
+      {/* <Footer /> */}
     </div>
   );
 }
