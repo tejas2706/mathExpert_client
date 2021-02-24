b6d2c0cad9c6c86782865220c816908b2dc8083d
tejaslad <tejaslad2706@gmail.com>
2021-02-25 02:12:56 +0530

    added about us page and form

--- src/App.js
+++ src/App.js
@@ -10,20 +10,32 @@ import QuestionsCard from './Components/QuestionsCard';
 import QuizQuestion from './Components/QuizQuestion';
 import Quiz from './Pages/Quiz';
 import QuizResult from './Pages/QuizResult';
+import AboutUs from './Pages/AboutUs';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app__root">
+    <Router>
+      <div className="app__root">
+      {/* <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/topicDetails" component={TopicDetails}/>
+        <Route path="/questions" component={Questions}/>
+        <Route path="/quizResult" component={QuizResult}/>
+        <Route path="/quiz" component={Quiz}/>
+        <Route path="/" component={Home}/>
+      <QuizResult />
+      </Switch> */}
       {/* <Navbar /> */}
-      {/* <TopicDetails /> */}
       {/* <Home/> */}
       {/* <Questions /> */}
       {/* <Quiz /> */}
       {/* <QuizQuestion /> */}
       {/* <Quiz /> */}
-      <QuizResult />
       {/* <Footer /> */}
+      <AboutUs />
     </div>
+    </Router>
   );
 }
 
---
+++ src/Pages/AboutUs/index.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import face1 from '../../assets/face.png';
+import face2 from '../../assets/face12.png';
+import './styles.css';
+
+
+
+function AboutUs() {
+    
+    const [name, setname] = useState("");
+    const [emailId, setemailId] = useState("");
+    const [phoneNo, setphoneNo] = useState("");
+    const [city, setcity] = useState("");
+    const [message, setmessage] = useState("");
+
+    const handleSubmit = (event) => {
+        let response = { name, emailId, phoneNo, city, message };
+        alert('Response was submitted');
+        event.preventDefault();
+    }
+
+
+    return (
+        <div className="aboutUs__container">
+            <div className="aboutUs__innerContainer">
+                <div className="aboutUs__aboutUs">
+                    <h1 className="aboutUs__title">About Us</h1>
+                    <div className="aboutUs__content">
+                        We,
+                        <img alt="tejas" src={face1} className="aboutUs__image"></img>
+                        <img src={face2} alt="murtuza" className="aboutUs__image"></img>
+                        have created MathExpert with the intent of preparing the students for Olympiad exams and help them to do
+                        their best in each and every topic of Mathematics. We provide detailed explanation that includes video and audio of all
+                        sorts of problem sums categoried by the topics and classes.
+                    </div>
+                </div>
+                <div className="aboutUs__contactUs">
+                    <h1>Contact Us</h1>
+                    <form className="aboutUs__contactUsForm" onSubmit={handleSubmit}>
+                        <input placeholder="name" className="aboutUs__inputField" type="text" value={name} onChange={e => setname(e.target.value)} />
+                        <input placeholder="email Id" className="aboutUs__inputField" type="text" value={emailId} onChange={e => setemailId(e.target.value)} />
+                        <input placeholder="contact number" className="aboutUs__inputField" type="text" value={[phoneNo]} onChange={e => setphoneNo(e.target.value)} />
+                        <input placeholder="city" className="aboutUs__inputField" type="text" value={city} onChange={e => setcity(e.target.value)} />
+                        <textarea placeholder="message" className="aboutUs__inputField_textarea" type="text" value={message} onChange={e => setmessage(e.target.value)} />
+                        <input type="submit" className="aboutUs__submit" value="Submit" />
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default AboutUs
