8f01dc24a6b9caa54c786676d5d7f276c899b7e1
Murtuza B <murtuzabagasrawala@gmail.com>
2021-02-27 12:56:47 +0530

    Quiz page modified with timer and questions attempted block

--- src/Components/QuizQuestion/index.js
+++ src/Components/QuizQuestion/index.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import "./styles.css"
 
-export default function QuizQuestion() {
-    let questionNumber = 1;
-    let question = "Enter your name and size to sign up for a T-Shirt."
-    let options = [1,2,3,4]
+export default function QuizQuestion({questionDetails, questionNumber, setquestionsAnswered,questionsAnswered}) {
+    const handleOptionClick = (optionIndex) => {
+        setquestionsAnswered({...questionsAnswered, [questionDetails.questionId]: optionIndex})
+    }
+
     return (
         <div className="quizQuestion__container">
             <div className="quizQuestion__question">
                 <div className="quizQuestion__questionText">
-                    <h3>{`Q. ${questionNumber} :  ${question}`}</h3>
+                    <h3>{`Q. ${questionNumber + 1} :  ${questionDetails.question}`}</h3>
                 </div>
             </div>
             <div className="quizQuestion__options">
-                {options.map((eachOption)=> {
-                    return (<div className="quizQuestion__eachOption">
+                {questionDetails.options.map((eachOption, index)=> {
+
+                    
+
+
+                    return (<div className={ `quizQuestion__eachOption ${questionsAnswered.hasOwnProperty([questionDetails.questionId]) && questionsAnswered[questionDetails.questionId] === index ? "quizQuestion_optionSelected" : "" } `} onClick={() => handleOptionClick(index)}>
                             <h3>{eachOption}</h3>
                     </div>);
                 })}
--- src/Pages/Quiz/index.js
+++ src/Pages/Quiz/index.js
@@ -1,26 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./styles.css"
 import QuizQuestion from "../../Components/QuizQuestion";
 import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import Timer from 'react-compound-timer'
+import AssignmentTurnedInIcon from '@material-ui/icons/AssignmentTurnedIn';
+import QuestionsList from "./dummyQuestions"
 
 export default function Quiz() {
-    let quizQuestion = [1,2,3,4,5,6,7,8]
+    let maxScore = 80;
+    const [questionsAnswered, setquestionsAnswered] = useState({})
     return (
         <div className="quiz__container">
             <div className="quiz__header">
                 <h3><span>Topic: </span>Quadrilaters / squares</h3>
                 <div className="quiz__duration">
+                    <AssignmentTurnedInIcon />
+                    <h3>&nbsp;{`Max Score: ${maxScore}`}</h3>&nbsp;&nbsp;
                     <AccessTimeIcon />
-                    <h3>&nbsp;Duration: 30 mins</h3>
+                    <h3>&nbsp;Time Left:</h3>&nbsp;
+                    <div>
+                        <h3>
+                            <Timer
+                                initialTime={100000}
+                                direction="backward"
+                                checkpoints={[
+                                    {
+                                        time: 0,
+                                        callback: () => alert('Times Up'),
+                                    },
+                                ]}
+                            >
+                                {() => (
+                                    <div>
+                                        <Timer.Hours /> :&nbsp;
+                                        <Timer.Minutes /> :&nbsp;
+                                        <Timer.Seconds />
+                                    </div>
+                                )}
+                            </Timer>
+                        </h3>
+                    </div>
                 </div>
             </div>
-            <div className="quiz__content">
-                {quizQuestion.map((eachQuestion) => {
-                    return (<div className="quiz__eachQuestion">
-                        <QuizQuestion />
-                    </div>);
+            <div className="quiz__contentContainer">
+                <div className="quiz__content">
+                    {QuestionsList.map((eachQuestion, index) => {
+                        return (<div className="quiz__eachQuestion">
+                            <QuizQuestion questionDetails={eachQuestion} questionNumber={index} setquestionsAnswered={setquestionsAnswered} questionsAnswered={questionsAnswered} />
+                        </div>);
                     })
-                }
+                    }
+                </div>
+                <div className="quiz__questionBlock">
+                    <div className="quiz__questionsBlock__container">
+                        <h3>Questions . . . </h3>
+                        <div className="quiz__questionsBlock__questions">
+                            {
+                                QuestionsList.map((each,index) => {
+                                    return (
+                                        <div className={`quiz__questionsBlock__eachQues ${questionsAnswered.hasOwnProperty(each.questionId) && "quiz__questionsBlock_eachQues_answered"}` }>
+                                            {index + 1}
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className="quiz__submit">
                 <button className="quiz__submitBtn">Submit</button>
